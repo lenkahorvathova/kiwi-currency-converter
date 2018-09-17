124e0c197754fbdf73c78aed1f105133f0b2be70
Lenka Horvathova <lenka.m.horvathova@gmail.com>
2018-09-16 20:25:34 -0400

    Implemented an API application.

---
+++ converter/libs/currency_converter.py
@@ -0,0 +1,81 @@
+from forex_python.converter import CurrencyRates, RatesNotAvailableError
+
+
+class CurrencyConverter:
+    currency_symbols = {
+        "$": "USD",
+        "£": "GBP",
+        "€": "EUR",
+        "¥": "CNY"
+    }
+
+    @staticmethod
+    def convert(amount, input_currency, output_currency):
+        if not CurrencyConverter.is_valid(amount, input_currency, output_currency):
+            return CurrencyConverter.format_output(
+                amount,
+                input_currency,
+                CurrencyConverter.error_message("Invalid input. Check arguments!"))
+
+        input_currency = CurrencyConverter.resolve_symbol(input_currency)
+        output_currency = CurrencyConverter.resolve_symbol(output_currency)
+
+        output = CurrencyConverter.prepare_output(amount, input_currency, output_currency)
+
+        result = CurrencyConverter.format_output(amount, input_currency, output)
+        return result
+
+    @staticmethod
+    def is_valid(amount, input_currency, output_currency):
+        if amount < 0:
+            return False
+        if not input_currency:
+            return False
+        if input_currency \
+                and len(input_currency) == 1 \
+                and input_currency not in CurrencyConverter.currency_symbols:
+            return False
+        if output_currency \
+                and len(output_currency) == 1 \
+                and output_currency not in CurrencyConverter.currency_symbols:
+            return False
+        return True
+
+    @staticmethod
+    def prepare_output(amount, input_currency, output_currency):
+        if output_currency:
+            try:
+                value = CurrencyRates().convert(input_currency, output_currency, amount)
+                value = "{0:.2f}".format(value)
+            except RatesNotAvailableError:
+                return CurrencyConverter.error_message("Invalid input or output currency.")
+
+            output = {output_currency: value}
+        else:
+            rates = CurrencyRates().get_rates(input_currency)
+
+            for currency_code in rates:
+                rates[currency_code] = "{0:.2f}".format(rates[currency_code] * amount)
+            output = rates
+
+        return output
+
+    @staticmethod
+    def resolve_symbol(currency):
+        if currency and len(currency) == 1:
+            currency = CurrencyConverter.currency_symbols[currency]
+        return currency
+
+    @staticmethod
+    def error_message(message):
+        return {"error": message}
+
+    @staticmethod
+    def format_output(amount, input_currency, output):
+        return {
+            "input": {
+                "amount": amount,
+                "currency": input_currency
+            },
+            "output": output
+        }
--- converter/views.py
+++ converter/views.py
@@ -1,5 +1,21 @@
 from django.http import JsonResponse
+from converter.libs.currency_converter import CurrencyConverter
 
 
 def index(request):
-    return JsonResponse({'test': 'ok'})
+    if request.method == 'GET':
+        amount = request.GET.get('amount', None)
+        input_currency = request.GET.get('input_currency', None)
+        output_currency = request.GET.get('output_currency', None)
+
+        if amount:
+            try:
+                amount = float(amount)
+            except ValueError:
+                return JsonResponse(
+                    CurrencyConverter.error_message('Amount "%s" is an invalid float value.' % amount),
+                    status=400)
+
+        result = CurrencyConverter.convert(amount, input_currency, output_currency)
+
+        return JsonResponse(result, status=200)
